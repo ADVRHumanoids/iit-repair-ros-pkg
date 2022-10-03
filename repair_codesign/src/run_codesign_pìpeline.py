cd18f8e18d12e73dda78d6f31166bd6808422289
andrea patrizi <andrepatrizi1b6e6@gmail.com>
2022-10-03 12:44:38 +0200

    just some name change

--- repair_codesign/src/run_codesign_pìpeline.py
+++ repair_codesign/src/run_codesign_pìpeline.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     # first level specific arguments
     parser.add_argument('--multistart_n_l1', '-msn_l1', type = int,\
                         help = '', default = 108)
-    parser.add_argument('--max_trials_factor_l1', '-mtfl1', type=int,\
+    parser.add_argument('--max_trials_factor_s1', '-mtfs1', type=int,\
                         help = 'for each multistart node, at best max_trials_factor new solutions will be tried to obtain an optimal solution',
                         default = 10)
 
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     parser.add_argument('--is_biman_pick', '-ibp', type=str2bool,\
                         help = 'whether to use bimanual pick task', default = True)
 
-    parser.add_argument('--ig_seed_l1', '-ig_l1', type = int,\
+    parser.add_argument('--ig_seed_s1', '-is_s1', type = int,\
                         help = '', default = 547)
     parser.add_argument('--ipopt_verb_lev', '-ipopt_v', type = int,\
                         help = '', default = 1)
@@ -59,22 +59,22 @@ if __name__ == '__main__':
                         help = 'whether to use the static tau minimization cost', default = False)
 
     # second level-specific arguments
-    parser.add_argument('--multistart_n_l2', '-msn_l2', type=int,\
+    parser.add_argument('--multistart_n_s3', '-msn_s3', type=int,\
                         help = 'number of multistarts (per cluster) to use',
                         default = 20)
-    parser.add_argument('--max_trials_factor_l2', '-mtfl2', type=int,\
+    parser.add_argument('--max_trials_factor_s3', '-mtfl3', type=int,\
                         help = 'for each multistart node, at best max_trials_factor new solutions will be tried to obtain an optimal solution',
                         default = 20)
-    parser.add_argument('--n_clust_l2', '-nc_l2', type=int,\
+    parser.add_argument('--n_clust_s2', '-nc_s2', type=int,\
                         help = 'number of clusters to be generated', default = 5)
-    parser.add_argument("--ig_seed_l2", '-ig_l2', type = int,\
+    parser.add_argument("--ig_seed_s3", '-ig_s3', type = int,\
                         help = '', default = 129)
     
     # cl man reference generation-specific arguments
     # parser.add_argument('--gen_cl_man_ref', '-gen_clmr', type=bool,\
     #                     help = 'whether to run the cl. manipulability reference generation',
     #                     default = True)
-    # parser.add_argument('--max_trials_factor_clmr', '-mtfl2', type=int,\
+    # parser.add_argument('--max_trials_factor_clmr', '-mtfl3', type=int,\
     #                     help = 'for each multistart node, at best max_trials_factor new solutions will be tried to obtain an optimal solution',
     #                     default = 20)
 
@@ -140,9 +140,9 @@ if __name__ == '__main__':
                                         "-mst", \
                                         str(args.multistart_n_l1), \
                                         "-mtf", \
-                                        str(args.max_trials_factor_l1), \
+                                        str(args.max_trials_factor_s1), \
                                         "-igs", \
-                                        str(args.ig_seed_l1),  \
+                                        str(args.ig_seed_s1),  \
                                         "-fnn", \
                                         str(args.filling_nnodes), \
                                         "-ipopt_v", \
@@ -187,7 +187,7 @@ if __name__ == '__main__':
             print(colored('\n An exception occurred while running the first level of the codesign pipeline. Muy malo!!! \n', "red"))
     
 
-    if args.multistart_n_l2 > 0:
+    if args.multistart_n_s3 > 0:
 
         try:
 
@@ -206,15 +206,15 @@ if __name__ == '__main__':
                                         "-ipopt_v", \
                                         str(args.ipopt_verb_lev), \
                                         "-nc",\
-                                        str(args.n_clust_l2), 
+                                        str(args.n_clust_s2), 
                                         "-ma57", \
                                         str(args.use_ma57), 
                                         "-igs", \
-                                        str(args.ig_seed_l2),
+                                        str(args.ig_seed_s3),
                                         "-mtf", \
-                                        str(args.max_trials_factor_l2), \
+                                        str(args.max_trials_factor_s3), \
                                         "-mst",
-                                        str(args.multistart_n_l2), \
+                                        str(args.multistart_n_s3), \
                                         "-urdf", \
                                         urdf_full_path, \
                                         "-coll", \
