049eadca5fa9250112fa92fdca10df6863f3099f
andrea patrizi <andreapatrizi1b6e6@gmail.com>
2023-02-02 18:59:40 +0100

    added srdfs and ignore joints macro for the softhand

---
+++ repair_srdf/srdf/generate_srdfs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+xacro repair_full.srdf.xacro -o repair_full.srdf
+
+
+ 
--- repair_urdf/urdf/generate_urdfs.sh
+++ repair_urdf/urdf/generate_urdfs.sh
@@ -5,6 +5,3 @@
 # the urdf
 
 xacro repair_full.urdf.xacro is_sliding_wrist:=true gen_coll:=false load_sol:=true use_updated_wrist:=false -o repair_full.urdf
-
-
- 
